818014969e6b66b334bd0d98e001ade618819b12
emeguskim <imanol.eguskiza.rojo@everis.com>
2015-07-07 16:53:43 +0200

    Fixed phpcs errors on theme

--- themes/inea/templates/page.tpl.php
+++ themes/inea/templates/page.tpl.php
@@ -2,7 +2,7 @@
 
 /**
  * @file
- * ec_resp's theme implementation to display a single Drupal page.
+ * Ec_resp's theme implementation to display a single Drupal page.
  *
  * The doctype, html, head and body tags are not in this template. Instead they
  * can be found in the html.tpl.php template normally located in the
