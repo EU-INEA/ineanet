366f8eccd273ca1e6ea2ddefe2a42eb7c5c97adf
Anthony Maciejowski <Anthony.Maciejowski@ext.ec.europa.eu>
2016-11-10 10:39:03 +0100

    Multisite-12619 code formatting

--- lib/themes/inea/templates/print--pdf.tpl.php
+++ lib/themes/inea/templates/print--pdf.tpl.php
@@ -73,50 +73,54 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0"
       dir="<?php print $language->dir; ?>">
 <head>
-    <?php print $head; ?>
-    <base href='<?php print $url ?>'/>
-    <title><?php print $print_title; ?></title>
-    <?php print $scripts; ?>
-    <?php if (isset($sendtoprinter)) print $sendtoprinter; ?>
-    <?php print $robots_meta; ?>
-    <?php if (theme_get_setting('toggle_favicon')): ?>
-        <link rel='shortcut icon' href='<?php print theme_get_setting('favicon') ?>' type='image/x-icon'/>
-    <?php endif; ?>
-    <?php print $css; ?>
+  <?php print $head; ?>
+  <base href='<?php print $url ?>'/>
+  <title><?php print $print_title; ?></title>
+  <?php print $scripts; ?>
+  <?php if (isset($sendtoprinter)) {
+    print $sendtoprinter;
+  } ?>
+  <?php print $robots_meta; ?>
+  <?php if (theme_get_setting('toggle_favicon')): ?>
+    <link rel='shortcut icon' href='<?php print theme_get_setting('favicon') ?>' type='image/x-icon'/>
+  <?php endif; ?>
+  <?php print $css; ?>
 </head>
 <body>
-
 <htmlpagefooter name="MyFooter1">
-    <table width="100%" style="margin-top:25mm;">
-        <tr>
-            <td width="43%"></td>
-            <td width="20%" class="footer-pdf"></td>
-            <td width="37%"></td>
-        </tr>
-    </table>
+  <table width="100%" style="margin-top:25mm;">
+    <tr>
+      <td width="43%"></td>
+      <td width="20%" class="footer-pdf"></td>
+      <td width="37%"></td>
+    </tr>
+  </table>
 </htmlpagefooter>
 <sethtmlpagefooter name="MyFooter1" value="on"/>
-
 <?php if (!empty($message)): ?>
-    <div class="print-message"><?php print $message; ?></div><p/>
+  <div class="print-message"><?php print $message; ?></div><p/>
 <?php endif; ?>
 <?php if ($print_logo): ?>
-    <div class="print-logo"><?php print $print_logo; ?></div>
+  <div class="print-logo"><?php print $print_logo; ?></div>
 <?php endif; ?>
 <div class="print-site_name"><?php print theme('print_published'); ?></div>
 <p/>
 <div class="print-breadcrumb"><?php print theme('print_breadcrumb', array('node' => $node)); ?></div>
 <hr class="print-hr"/>
 <?php if (!isset($node->type)): ?>
-    <h2 class="print-title"><?php print $print_title; ?></h2>
+  <h2 class="print-title"><?php print $print_title; ?></h2>
 <?php endif; ?>
 <div class="print-content"><?php print $content; ?></div>
 <div class="print-footer"><?php print theme('print_footer'); ?></div>
 <hr class="print-hr"/>
 <?php if ($sourceurl_enabled): ?>
-    <div class="print-source_url">
-        <?php print theme('print_sourceurl', array('url' => $source_url, 'node' => $node, 'cid' => $cid)); ?>
-    </div>
+  <div class="print-source_url">
+    <?php print theme('print_sourceurl', array
+      ( 'url' => $source_url,
+        'node' => $node,
+        'cid' => $cid)
+      ); ?>
+  </div>
 <?php endif; ?>
 <div class="print-links"><?php print theme('print_url_list'); ?></div>
 <?php print $footer_scripts; ?>
